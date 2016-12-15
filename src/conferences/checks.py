a6193962b4a32dd9171425b84f459d90e0069c50
Tzu-ping Chung <uranusjr@gmail.com>
2016-12-15 14:54:25 +0800

    Allow system checking with app_configs=None

--- src/conferences/checks.py
+++ src/conferences/checks.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from django.core import checks
 
 
@@ -6,23 +7,28 @@ def default_conference_check(app_configs, **kwargs):
     """
     errors = []
 
-    for app_config in app_configs:
-        if app_config.label == 'conferences':
-            Conference = app_config.get_model('Conference')
-            break
+    model = None
+    if app_configs is None:
+        model = apps.get_model('conferences.Conference')
+    else:
+        for app_config in app_configs:
+            if app_config.label == 'conferences':
+                model = app_config.get_model('Conference')
+                break
 
-    try:
-        Conference.objects.get_default()
-    except Conference.DoesNotExist:
-        errors.append(
-            checks.Error(
-                'invalid default conference',
-                hint=(
-                    'Default conference Does not exist. Check your '
-                    'CONFERENCE_DEFAULT_SLUG setting, and make sure a '
-                    'conference with matching slug exists.'
-                ),
-                id='conferences.E001',
+    if model is not None:
+        try:
+            model.objects.get_default()
+        except model.DoesNotExist:
+            errors.append(
+                checks.Error(
+                    'invalid default conference',
+                    hint=(
+                        'Default conference Does not exist. Check your '
+                        'CONFERENCE_DEFAULT_SLUG setting, and make sure a '
+                        'conference with matching slug exists.'
+                    ),
+                    id='conferences.E001',
+                )
             )
-        )
     return errors
